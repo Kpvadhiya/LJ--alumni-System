3e504ff7c9a0248576ce9833326dd800ced8a8a3
Keyur Vadhiya <131326256+Kpvadhiya@users.noreply.github.com>
2023-07-13 21:40:51 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,38 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <?php echo html_entity_decode($_SESSION['system']['about_content']) ?>        
+            LJ Group of Institutes is managed by Lok Jagruti Kendra (LJK), a Charitable Trust and a Registered Society established in1980 by eminent academicians and visionaries like Prof. B.M. Peerzada, former Dean of Commerce Faculty,Gujarat University, Padma Bhushan Lord Meghnad Desai (L ondon School of Economics ), Prof. Gautam Appa (LSE), Late Prof. M.S. Trivedi, former Vice Chancellor, South Gujarat University, renowned jurist Late Shri Girishbhai Patel and Shri Subodhbhai Shah. It was envisioned as “a key player in education and social development by promoting and nurturing creativity, scholarship, innovation and excellence through a chain of quality institutes.”  LJK’s mission has been “to establish and manage institutions with an environment in which new ideas, delivery strategies and scholarship flourish and from where leaders and innovators of tomorrow shall emerge.” 
+
+
+LJK runs 27 institutions on two well-developed environment-friendly campuses with dedicated buildings and infrastructure facilities, and offering various diploma, undergraduate and postgraduate programmes duly approved by respective apex bodies. LJ Institutes have over 18000 students and 700+ faculties engaged in teaching-learning, research and extension activities and striving to develop students into complete citizens not only having the necessary subject knowledge and skills, but also the empathy towards various environmental, social, cultural and other issues affecting the society.<br><br>
+
+
+<li>Objectives</li>
+
+<br>
+<ul> Affordability: Affordable higher education of high quality in Gujarat</ul>
+
+<ul> Contribution: Contribute in a big way to local industrial need and social development</ul>
+
+<ul> Skill Development: Focus on skill development, employability and holistic development</ul>
+
+<ul> Social Responsibility: Become a responsible social institution through value creation in the region</ul>
+
+<ul> Inclusion: Inclusive education</ul>
+
+<ul> Pedagogy: Lab-to-land-to-lab pedagogy</ul>
+        </div>
+        </section>
